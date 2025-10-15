481cf53269a772d58bf4ebd056da38b4ba343e10
José Hernandes <josehhernandescardoso@gmail.com>
2025-10-15 10:05:06 -0300

    Revert "New type 'Stkiers' on tad_collection.py"
    Reverte o commit anterior (c6df1ca8c12d3dffbb9b8dfd0279ac7022911f43).
    Foi verificado que o passo anterior foi realizado de forma preciptada e
    dificultaria o uso do TAD.

--- tad_collection.py
+++ tad_collection.py
@@ -1,25 +1,10 @@
 from __future__ import annotations
-from dataclasses import dataclass
-
-@dataclass
-class Stikers():
-    '''
-    Um grupo de figurinhas do mesmo tipo de um determinado álbum
-    que contém N figurinhas distintas.
-
-    *code*: é o código que determina o tipo das figurinhas.
-            Varia de 0 a N.
-    *over*: é a quantidade de figurinhas repetidas além da primeira.
-            É sempre maior igual a 0.
-    '''
-    code: int
-    over: int
 
 class Collection:
     '''
     Uma coleção de figurinhas de um determinado álbum.
-    Indica quais e quantas figurinhas o colecionador possui,
-    além do máximo de figurinhas distintas que o álbum tem.
+    Indica quais e quantas figurinhas o colecionador possui, além
+    do máximo de figurinhas distintas que o álbum tem.
 
     Exemplos:
 
@@ -28,25 +13,26 @@ class Collection:
     def __init__(self) -> None:
         raise NotImplementedError
     
-    def insert(self, stiker: Stikers) -> None:
+    def insert(self, code: int) -> None:
         '''
-        Adiciona a figurinha *stiker* ao álbum.
-        Se ela já estiver no álbum, aumenta a quantia repetida em 1.
+        Aumenta em 1 a quantidade da figurinha de código *code*.
+
         Se ela não estiver na coleção, a figurinha é adicionada.
         '''
         raise NotImplementedError
 
-    def remove(self, stiker: Stikers) -> None:
+    def remove(self, code: int) -> None:
         '''
-        Reduz em 1 a quantidade das figurinhas repetidas do tipo *stiker*.
-        Se a quantidade de repetidas for 0, a figurinha é removida da coleção.
-        Se a figurinha não estiver na coleção, nada acontece.
+        Reduz em 1 a quantidade da figurinha de código *code*.
+
+        Se a quantidade da figurinha redizir para 0, ela é removida
+        da coleção. Se a figurinha não estiver na coleção, nada acontece.
         '''
         raise NotImplementedError
     
-    def have(self, stiker: Stikers) -> bool:
+    def have(self, code: int) -> bool:
         '''
-        Retorna True se alguma figurinha do tipo *stiker* está na coleção.
+        Retorna True se a figurinha de código *code* está na coleção.
         Retorna False em caso contrário.
         '''
         raise NotImplementedError
